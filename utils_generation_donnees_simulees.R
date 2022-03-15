0a465666c2223b1e58e3898cd1a36725cbf6dad8
PapaYoun <pierre.gloaguen@agroparistech.fr>
2022-03-15 14:08:11 +0100

    nouveau setting de simu

--- utils_generation_donnees_simulees.R
+++ utils_generation_donnees_simulees.R
@@ -1,12 +1,13 @@
 ## ----librariries-----------------------------------------------------
 library(tidyverse) # Pour la manipulation de données
-library(MASS) 
 
 ## ----simulation de Y-------------------------------------------------
 # On fixe les dimensions 
 n = 100
 p = 10
-k_etoile = 2
+k_etoile = 3
+q <- 3 # Nombre de colonnes de X
+
 
 # Pour avoir les mêmes données
 set.seed(123)
@@ -14,7 +15,7 @@ set.seed(123)
 # On choisit sigma
 sigma = diag(10**-2, p)
 # On simule les epsilon
-epsilon = mvrnorm(n, rep(0,p), sigma)
+epsilon = MASS::mvrnorm(n, rep(0,p), sigma)
 
 #On fixe lambda
 lambda = matrix(sample(-1:1, 
@@ -23,11 +24,10 @@ lambda = matrix(sample(-1:1,
                 nrow = p, ncol = k_etoile)
 
 #On simule les eta
-eta = mvrnorm(n, rep(0,k_etoile), diag(1,k_etoile))
+eta = MASS::mvrnorm(n, rep(0,k_etoile), diag(1,k_etoile))
 
-q <- 3
 
-X = mvrnorm(n, rep(0, q), diag(1, q))
+X = MASS::mvrnorm(n, rep(0, q), diag(1, q))
 
 beta <- matrix(sample(-1:1, 
               size = p * q, 
